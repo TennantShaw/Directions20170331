394c459af4bf1da52805f05c2d5ef0e65c99b0ce
Tennant Shaw <tennantshaw@macs-mbp.lan>
2017-03-31 15:22:05 -0400

    Initial Commit

---
+++ Directions20170331/Directions20170331.h
@@ -0,0 +1,19 @@
+//
+//  Directions20170331.h
+//  Directions20170331
+//
+//  Created by Tennant Shaw on 3/31/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Directions20170331.
+FOUNDATION_EXPORT double Directions20170331VersionNumber;
+
+//! Project version string for Directions20170331.
+FOUNDATION_EXPORT const unsigned char Directions20170331VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Directions20170331/PublicHeader.h>
+
+
